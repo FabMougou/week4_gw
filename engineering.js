5f1b7b1bf4c8bbb0fdda8d891b2b6c4d957b78de
FabMougou <fabricio.mougou@gmail.com>
2023-10-30 00:26:12 +0000

    Deleted unused pages. Developed engineering page and made title animation

---
+++ engineering.js
@@ -0,0 +1,41 @@
+const engineeringTypes = ["CIVIL", "MECHANICAL", "CHEMICAL", "AEROSPACE", "ELECTRICAL"];
+
+let typeIndex = 1;
+let currentTitleText = document.getElementById("replace-text-effect");
+
+
+
+function ChangeText() {
+    currentTitleText.textContent = engineeringTypes[typeIndex];
+
+    setTimeout(flyTop, 200);
+
+}
+
+function flyTop() {
+    console.log("ran");
+
+    currentTitleText.style.transform = 'translate(0%,-100px)';
+
+    setTimeout(flyBottom,1000);
+
+}
+
+function flyBottom() {
+
+    if (typeIndex != 4){
+        typeIndex++;
+    }
+    else {
+        typeIndex = 0;
+    }
+
+    currentTitleText.textContent = engineeringTypes[typeIndex];
+    currentTitleText.style.transform = 'translate(0%, 0px)';
+
+    setTimeout(flyTop,3000);
+    
+
+}
+
+ChangeText();
